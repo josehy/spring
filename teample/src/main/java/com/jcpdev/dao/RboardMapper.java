eb918467f106dfd140793062d1bbf9fcb5d9c755
josehy <tjdgustjdwn@naver.com>
2021-12-09 18:43:41 +0900

    예약게시판 controller, dao, model, service구현

---
+++ teample/src/main/java/com/jcpdev/dao/RboardMapper.java
@@ -0,0 +1,24 @@
+package com.jcpdev.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import com.jcpdev.model.Member;
+import com.jcpdev.model.PageDto2;
+import com.jcpdev.model.Rboard;
+
+
+public interface RboardMapper {
+	List<Rboard> getList(PageDto2 dto);
+	Rboard selectByIdx(int r_idx);
+	int getCount();
+	int update(Rboard dto);
+	int delete(int r_idx);
+	void readCount(int r_idx);
+	int insert(Rboard rboard);
+	int resultCount(String nick);
+	List<Member> psByNick(PageDto2 dto);
+	List<Member> getPsList(PageDto2 dto);
+	int psCount();
+	int searchCount(Map<String,Object>map);
+}
